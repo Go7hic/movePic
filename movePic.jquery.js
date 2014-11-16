4266b40f244de66b6338b7708fb7c32a80792d9a
Go7hic <gtfx0209@qq.com>
2014-11-17 01:59:02 +0800

    Create movePic.jquery.js

---
+++ movePic.jquery.js
@@ -0,0 +1,131 @@
+(function($) {
+    var defaults = {
+        cursor: "move",
+        cursorColor: "ffd24d",
+        cursorBorder: "ff8000",
+        cursorOpacity: "0.5",
+
+        layerwidth: 400,
+        layerheight: 300,
+        layerborder: #ddd;
+        fade: true;
+
+        layerwidth: 1280;
+        layerheight: 960;
+    }
+
+    var enlarge = function(option) {
+        option = $.extend({}, defaults, option); 
+        $(this).each(function() {
+            var self = $(this).css("position","relative");
+            var img = self.children().first();
+
+            var ratio = {
+                x: img.width()/option.largewidth,
+                y: img.height()/option.largeheight
+            }
+            var ratio = {
+                x: img.width()/option.largewidth,
+                y: img.height()/option.largeheight
+            }
+            var size = {
+                shade: {
+                    width: option.layerwidth*ratio.x - 2,
+                    height: option.layerheight.height*ratio.y - 2
+                }
+            }
+
+            var shade = $("<div>").css({
+                "postion": "absolute",
+                "left": "0px",
+                "top": "0px",
+                "background-color": option.shadecolor,
+                "border": "1px solid" + option.shadeborder,
+                "width": size.shade.width,
+                "height"； size.shade.height,
+                "opacity": option.shadeopacity,
+                "cursor": option.cursor
+            });
+
+            sahde.hide().appendTo(self);
+
+            var large = $("<img>").css({
+                "postion": "absolute",
+                "display": "block",
+                "width": option.largewidth,
+                "height": option.largeheight,
+
+            });
+
+            var layer = $("<div>").css({
+                "postion": "absolute",
+                "left": self.width() + 5,
+                "top": 0,
+                "background-color": #111,
+                "overflow": "hidden",
+                "width": option.layerwidth,
+                "height": option.layerheight,
+                "border": "1px solid" + option.layerborder
+            });
+            large.attr("src", self.attr("href"));
+            large.appendTo(layer);
+            layer.hide().appendTo(self);
+
+            var half = {
+                x: size.sahde.width / 2,
+                y:size.shade.height / 2,
+            }
+
+            var area = {
+                width: self.innerWidth() - shade.outerWidth(),
+                height: self.innerHeight() - shade.outerHeight()
+            }
+
+            var ofset = self.offset();
+
+            var show = function() {
+                offset= self.offset();
+                sahde.show();
+                if(option.fade).layer.stop().fadeIn(300);
+                else layer.show();
+            }
+
+            var hide = function() {
+                shade.hide();
+                layer.hide();
+            }
+            self.mousemove(function(e) {
+                var x = e.pageX - offset.left;
+                var y = e.pageY - offset.top;
+
+                if(x < 0||x > self.innerWidth()) return hide();
+                if(y < 0 || y > self.innerHeight()) return hide();
+                
+                x = x - half.x;
+                y = y - half.y;
+                
+                if(x < 0) x = 0;
+                if(y < 0) y = 0;
+                if(x > area.width) x = area.width;
+                if(y > area.height) y = area.height;
+                
+                shade.css(
+                {
+                    left: x,
+                    top: y
+                });
+                
+                large.css(
+                {
+                    left: (0 - x / ratio.x),
+                    top: (0 - y / ratio.y)
+                });
+            })
+            .mouseenter(show);
+            .mouseleave(hide);
+        });
+    }
+    $.fn.extend({
+        enlarge: enlarge
+    })
+})(jQuery)
